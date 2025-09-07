24b8be2fa830f3dcfb1b6126c79818a7fda545e9
BoreyChet70 <borey.chan70@gmail.com>
2025-09-07 16:26:06 +0700

    update3

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,23 @@
 <HeroArea v-if="!remove"></HeroArea>
 <button @click="changeValue">Remove from DOM</button>
 <ServiceSection></ServiceSection>
+<AboutSection></AboutSection>
+<WhySection></WhySection>
+<TeamSection></TeamSection>
+<ClientSection></ClientSection>
+<InfoSection></InfoSection>
+<FooterSection></FooterSection>
 </template>
 
 <script>
 import HeroArea from './components/HeroArea.vue';
 import ServiceSection from './components/ServiceSection.vue';
+import AboutSection from './components/AboutSection.vue';
+import WhySection from './components/WhySection.vue';
+import TeamSection from './components/TeamSection.vue';
+import ClientSection from './components/ClientSection.vue';
+import InfoSection from './components/InfoSection.vue';
+import FooterSection from './components/FooterSection.vue';
 export default {
   data() {
     return {
@@ -23,6 +35,12 @@ export default {
   components: {
     HeroArea,
     ServiceSection,
-}
+    AboutSection,
+    WhySection,
+    TeamSection,
+    ClientSection,
+    InfoSection,
+    FooterSection,
+  }
 }
 </script>
