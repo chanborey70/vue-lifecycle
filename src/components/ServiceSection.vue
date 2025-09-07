fe086937180e759019bf8ea42e098c8950a7d389
BoreyChet70 <borey.chan70@gmail.com>
2025-09-07 15:59:03 +0700

    initial

---
+++ src/components/ServiceSection.vue
@@ -0,0 +1,64 @@
+<template>
+   <!-- service section -->
+
+   <section class="service_section layout_padding">
+    <div class="service_container">
+      <div class="container ">
+        <div class="heading_container heading_center">
+          <h2>
+            Our <span>Services</span>
+          </h2>
+          <p>
+            There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration
+          </p>
+        </div>
+        <div class="row">
+          <ServiceCard v-for="(service,index) in services"
+          :key="index"
+          :name ="service.name"
+          :description ="service.description"
+          :image ="service.image"
+          >
+        </ServiceCard>
+
+        </div>
+        <div class="btn-box">
+          <a href="">
+            View All
+          </a>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- end service section -->
+</template>
+<script>
+import ServiceCard from "@/components/cards/ServiceCard.vue";
+export default {
+  components: {
+    ServiceCard,
+  },
+  data() {
+    return {
+      services: [
+        {
+          name: "Currency Wallet",
+          description: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using",
+          image: "/images/s1.png",
+        },
+        {
+          name: "Security Storage",
+          description: "fact that a reader will be distracted by the readable content of a page when looking at its layout.The point of using",
+          image: "/images/s2.png",
+        },
+        {
+          name: "Expert Support",
+          description: "fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using",
+          image: "/images/s3.png",
+        },
+      ],
+    };
+  },
+}
+</script>
